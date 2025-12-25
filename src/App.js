b57de6841af3261b03348854e55bff8605b77a15
jarupat-jj <s6703052411236@email.kmutnb.ac.th>
2025-12-25 11:41:56 +0700

    classcomponent

--- src/App.js
+++ src/App.js
@@ -2,10 +2,16 @@
 import React from 'react'
 // import './style.css'
 import { Content, Footer, Header }from'./function-components';
-
+import Calendar from './class-components';
 function App() {
   return(
-    [<Header/>,<Content/>,<Footer/>]
+    <>
+    <Header/>
+    <p><center><Calendar/></center></p>
+    <Content/>
+    <Footer/>
+    </>
+    
     
   )
  
